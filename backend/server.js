06533420cc7e428713763638967c0474f21663a3
sarvgyaverma027@gmail.com <sarvgyaverma027@gmail.com>
2024-05-16 15:52:24 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,17 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 // File and route imports
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 
-
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server, io } from "./socket/socket.js";
+
+const __dirname = path.resolve();
 
 // Server setup
 
@@ -28,12 +30,11 @@ app.use("/api/messages", messageRoutes);
 
 app.use("/api/users", userRoutes);
 
-// app.get("/", (req, res) => {
-//   res.send("server is running!!");
-// });
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-// Mongoose stuff for advance
-
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/frontend", "dist", "index.html"));
+});
 
 // Server start
 server.listen(PORT, () => {
